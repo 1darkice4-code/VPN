773c788d2bdb9c8d5d097af24b7dbc38b65819a3
Pupa <overban2017@yandex.ru>
2025-10-26 12:10:31 +0300

    test13

---
+++ wg-init/00-create-wg0.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+CONFIG_FILE=/config/wg0.conf
+
+if [ ! -f "$CONFIG_FILE" ]; then
+  echo "Создаём wg0.conf..."
+  umask 077
+  PRIV_KEY=$(wg genkey)
+  PUB_KEY=$(echo "$PRIV_KEY" | wg pubkey)
+
+  cat <<EOF > $CONFIG_FILE
+[Interface]
+Address = 10.66.66.1/24
+ListenPort = 51830
+PrivateKey = $PRIV_KEY
+EOF
+
+  echo "Конфигурация wg0 создана"
+fi
